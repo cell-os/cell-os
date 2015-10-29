d134844b0e81109da948c282d2b4327fbe64a0d1
Cosmin Lehene <clehene@adobe.com>
2015-10-29 11:10:52 -0700

    [CELL-117] - troposphere build support

--- build.sh
+++ build.sh
@@ -10,10 +10,12 @@ rm -rf build
 mkdir build
 
 cp cell-os-base.yaml build/cell-os-base-${VERSION}.yaml
-cp deploy/aws/elastic-cell.json build/elastic-cell-${VERSION}.json
+./cell build
+cp deploy/aws/build/elastic-cell.json \
+  build/elastic-cell-${VERSION}.json
+cp deploy/aws/build/elastic-cell-scaling-group.json \
+  build/elastic-cell-scaling-group-${VERSION}.json
 cp cell build/cell
 cp build.sh build/
 cp VERSION build/
-
-
 
