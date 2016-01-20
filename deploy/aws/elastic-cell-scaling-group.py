f44739cd4d2aa6f83aada790df3faa6362f239cf
Cosmin Lehene <clehene@gmail.com>
2016-01-20 12:12:54 -0800

    [CELL-214] - Pin pip version to < 8

--- deploy/aws/elastic-cell-scaling-group.py
+++ deploy/aws/elastic-cell-scaling-group.py
@@ -483,6 +483,8 @@ yum install -y wget ruby ruby-devel kpartx parted
 # Pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
+# Downgrade pip to avoid https://github.com/pypa/pip/issues/3384
+pip install "pip==7.1.2"
 
 # aws-cli
 pip install --upgrade awscli
