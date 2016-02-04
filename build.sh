dc728e3c659698732153b11bdab1cac3e2692acd
Cosmin Lehene <clehene@adobe.com>
2016-02-03 20:55:09 -0800

    [CELL-253] - Build server script paths are broken after transition to new cell.py

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 # build script - meant to be run on build server
 # cleans up build directory and copies everything in a structure
 # that can be used for promotion
+# Typical use would be:
+#
+#   VERSION=$(cat VERSION)
+#   bash build.sh "https://s3.amazonaws.com/saasbase-repo/cell-os/\
+#   deploy/aws/elastic-cell-scaling-group-${VERSION}.json"
+
 set -e
 VERSION=$(cat VERSION)
 
@@ -14,13 +20,12 @@ rm -rf build
 mkdir build
 
 cp cell-os-base.yaml build/cell-os-base-${VERSION}.yaml
-./cell build $1
-cp deploy/aws/build/elastic-cell.json \
+./cell build build --template-url $1
+cp .generated/build/elastic-cell.json \
   build/elastic-cell-${VERSION}.json
-cp deploy/aws/build/elastic-cell-scaling-group.json \
+cp .generated/build/elastic-cell-scaling-group.json \
   build/elastic-cell-scaling-group-${VERSION}.json
-cp deploy/seed.tar.gz build/seed-${VERSION}.tar.gz
+cp .generated/build/seed.tar.gz build/seed-${VERSION}.tar.gz
 cp cell build/cell
-cp build.sh build/
 cp VERSION build/
 
