2631fae85f5a7413bab542c3adea54c54891fd27
Andrei Dragomir <adragomi@adobe.com>
2016-02-23 12:40:36 +0200

    [CELL-313] - kafka config endpoint fails (500) if broker added but not started

--- deploy/seed/99-cell/apigateway/api-gateway-config/scripts/lua/service/kafka.lua
+++ deploy/seed/99-cell/apigateway/api-gateway-config/scripts/lua/service/kafka.lua
@@ -5,7 +5,7 @@ local cjson = require "cjson"
 local _M = {}
 
 function _M.exec_default() 
-    -- pass through to defaults
+    -- pass through to defaults, go to the scheduler workload
 end
 
 function _M.exec_config()
@@ -13,14 +13,16 @@ function _M.exec_config()
         marathon_endpoint="http://marathon.gw." .. ngx.var.domain
     })
     local endpoints = m:endpoints_for_app(ngx.var.marathon_app_name)
+    -- we should have only 1 Kafka scheduler instance
     if #endpoints < 1 then
       ngx.header.content_type = "application/json; charset=utf-8"
       ngx.say(cjson.encode({ error = "Application has no endpoints" }))
       return ngx.exit(ngx.HTTP_OK)
     end
+
+    -- get the broker list from the scheduler
     local out = {}
     local scheduler_url = "http://" .. endpoints[1].host .. ":" .. endpoints[1].ports[1] .. "/api/broker/list"
-
     local hc = http.new()
     local res, err = hc:request_uri(scheduler_url, {
         method = "GET",
@@ -28,16 +30,20 @@ function _M.exec_config()
             ["Content-Type"] = "application/json",
         }
     })
+
     if err == nil and res.status == 200 then
         local brokers = cjson.decode(res.body)
+        -- brokers is a json array
+        -- started brokers have a "task" key inside the broker description
         for _, broker in ipairs(brokers.brokers) do
-            if broker.task.state == "running" then
+            if broker.task and broker.task.state == "running" then
                 table.insert(out, broker.task.endpoint)
             end
         end
     end
-    ngx.status = ngx.HTTP_OK  
+
     ngx.header.content_type = "application/json; charset=utf-8"  
+    ngx.status = ngx.HTTP_OK
     ngx.say(cjson.encode({ brokers = out }))  
     return ngx.exit(ngx.HTTP_OK)  
 end
