855117251633280e17dff067f9dc329fbdf42692
Andrei Dragomir <adragomi@adobe.com>
2016-08-26 14:59:17 +0300

    [CELL-546] - Pip packaging is broken by the config/ directory

--- setup.py
+++ setup.py
@@ -10,22 +10,27 @@ install_requirements = parse_requirements(
 requirements = [str(ir.req) for ir in install_requirements]
 version = open(os.path.join(here, 'VERSION')).readlines()[0].strip()
 
-data_dir = 'deploy/'
-data_files = [[os.path.join(root, f) for f in files]
-              for root, dirs, files in os.walk(data_dir)]
-data_files = [item.replace(data_dir, '') for l in data_files for item in l]
-data_files.append("../cell-os-base.yaml")
+def get_all_files(data_dir):
+    data_files = [[os.path.join(root, f) for f in files]
+                for root, dirs, files in os.walk(data_dir)]
+    data_files = [item.replace(data_dir, '') for l in data_files for item in l]
+    return data_files
 
 setup(
     name='cellos',
     version=version,
     author='metal-cell-dev@adobe.com',
     py_modules=['cell'],
-    packages=['deploy'],
     install_requires=requirements,
-    package_data={'deploy': data_files},
     include_package_data=True,
-    exclude_package_data={'deploy': ['aws/build*']},
+    zip_safe=False,
+    packages=['', 'deploy', 'config'],
+    package_dir={"deploy": "deploy", "config": "config"},
+    package_data = {
+        '': [ 'cell-os-base.yaml', 'VERSION', 'README.md', 'Dockerfile', 'requirements.txt'],
+        'deploy': get_all_files('deploy'),
+        'config': get_all_files('config')
+    },
     entry_points={
         'console_scripts': [
             'cell=cell:main',
